2c1a289bbc75d2c057896ff6cbe2bdc8fb8821a8
helloshyreckdc <shyreckdc@gmail.com>
2018-01-12 19:58:48 +0800

    create repository

---
+++ basic/hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# The first line is used to designate a interpreter, second line is coding
+name = input("What's your name?\n")
+print('Hello', name) # a comma can connect two strings and input a blank 
+print('100 + 200 =', 100 + 200)
+
+print('\\\t\\')
+print(r'\\\t\\') # r'' direct output
+
+# '''...''' can turn to new line without \n
+print('''line1
+line2
+line3''')
---
+++ basic/list_and_tuple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# The first line is used to designate a interpreter, second line is coding
+classmates = ['Michael', 'Bob', 'Tracy']
+print(classmates)
+len(classmates)
+print(classmates[0])
+print(classmates[-1])
+classmates.append('Adam')
+classmates.insert(1, 'Jack')
+print(classmates)
+classmates.pop()
+print(classmates)
+
